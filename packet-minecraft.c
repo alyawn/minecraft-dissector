9733418dbbba74eb02d928e0a81d39a6a68b097b
Alan De Smet <chaos@highprogrammer.com>
2011-02-15 21:31:33 -0600

    Copyright belongs in all source files (they all came from the same source)

--- packet-minecraft.c
+++ packet-minecraft.c
@@ -1,4 +1,6 @@
-/* Copyright (C) 2011 by Scott Brooks
+/* 
+Copyright (C) 2011 by Scott Brooks
+Copyright (C) 2011 by Alan De Smet
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
